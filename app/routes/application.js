f76ba0d115dda2060507f66955af0177bcc61680
Oliver Wilkie <oliverw@squareup.com>
2018-06-12 08:15:09 -0700

    I should be able to see a home page when logged in

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 
 export default Route.extend(ApplicationRouteMixin, {
   session: service(),
+  routeAfterAuthentication: 'authenticated.start',
   beforeModel() {
     if (this.get('session.isAuthenticated')) {
       this.transitionTo('authenticated.start');
