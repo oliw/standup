49871e599ed4a40c4ee8f15e314678d4ca245507
Oliver Wilkie <oliverw@squareup.com>
2018-07-24 21:54:49 -0700

    onUnload warn if unsaved changes

--- app/routes/standup.js
+++ app/routes/standup.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { on } from '@ember/object/evented';
 
 export default Route.extend({
   store: service(),
@@ -7,12 +8,24 @@ export default Route.extend({
   model(params) {
     return this.store.findRecord('standup', params.id);
   },
+
+  onUnloadMethod: () => {
+    if (this.controller.get('savePending')) {
+      return "You have unsaved changes, are you sure you want to leave?";
+    }
+  },
+
+  onUnload: on('init', function() {
+    $(window).bind('beforeunload', this.onUnloadMethod);
+  }),
+
   actions: {
     willTransition(transition) {
       if (this.controller.get('savePending') &&
         !confirm('You have unsaved changes, are you sure you want to leave?')) {
         transition.abort();
       } else {
+        $(window).unbind('beforeunload', this.onUnloadMethod);
         // Bubble the `willTransition` action so that
         // parent routes can decide whether or not to abort.
         return true;
