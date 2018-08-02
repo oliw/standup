168f6fdfa12d67808a70067c6440dceafdfb71c5
Oliver Wilkie <oliverw@squareup.com>
2018-08-02 08:56:24 -0700

    Improved basic layout of Standup page

--- app/models/standup.js
+++ app/models/standup.js
@@ -1,10 +1,13 @@
 import DS from 'ember-data';
 import DirtiableMixin from 'standup/mixins/dirtiable-mixin';
+import { equal, gt } from '@ember/object/computed';
 
 export default DS.Model.extend(DirtiableMixin, {
   title: DS.attr('string'),
   date: DS.attr('local-date'),
   creatorId: DS.attr('string'),
   createdAt: DS.attr('date'),
-  topics: DS.hasMany('topic')
+  topics: DS.hasMany('topic'),
+  hasNoTopics: equal('topics.length', 0),
+  hasSomeTopics: gt('topics.length', 0)
 });
--- app/models/topic.js
+++ app/models/topic.js
@@ -1,9 +1,12 @@
 import DS from 'ember-data';
 import DirtiableMixin from 'standup/mixins/dirtiable-mixin';
+import { notEmpty } from '@ember/object/computed';
 
 export default DS.Model.extend(DirtiableMixin, {
   subject: DS.attr('string'),
   yesterdays: DS.hasMany('entry'),
   todays: DS.hasMany('entry'),
-  blockers: DS.hasMany('entry')
+  blockers: DS.hasMany('entry'),
+
+  hasSubject: notEmpty('subject')
 });
